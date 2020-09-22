0e3c68fd5c2438971d989549e00a318fed403718
Steven Arcangeli <stevearc@stevearc.com>
2020-09-22 08:57:35 -0700

    Show document hierarchy

--- lua/aerial/callbacks.lua
+++ lua/aerial/callbacks.lua
@@ -1,6 +1,7 @@
 local config = require 'aerial.config'
 local data = require 'aerial.data'
 local nav = require 'aerial.navigation'
+local protocol = require 'vim.lsp.protocol'
 local window = require 'aerial.window'
 
 local M = {}
@@ -15,7 +16,7 @@ end
 
 M.symbol_callback = function(_, _, result, _, bufnr)
   if not result or vim.tbl_isempty(result) then return end
-  local items = vim.lsp.util.symbols_to_items(result, bufnr)
+  local items = M.symbols_to_items(result, bufnr)
   items = vim.tbl_filter(filter_symbol_predicate, items)
   table.sort(items, sort_symbol)
   local had_items = data.items_by_buf[bufnr] ~= nil
@@ -47,5 +48,54 @@ function M._buf_diagnostics_count(bufnr, kind)
   return count
 end
 
+-- Mostly copied from neovim source, with some tweaks for naming
+function M.symbols_to_items(symbols, bufnr)
+  --@private
+  local function _symbols_to_items(_symbols, _items, _bufnr, level)
+    for _, symbol in ipairs(_symbols) do
+      local kind = M._get_symbol_kind_name(symbol.kind)
+      local text = M._get_prefix(level, kind) .. symbol.name
+      if symbol.location then -- SymbolInformation type
+        local range = symbol.location.range
+        table.insert(_items, {
+          filename = vim.uri_to_fname(symbol.location.uri),
+          lnum = range.start.line + 1,
+          col = range.start.character + 1,
+          kind = kind,
+          text = text,
+        })
+      elseif symbol.range then -- DocumentSymbol type
+        table.insert(_items, {
+          filename = vim.api.nvim_buf_get_name(_bufnr),
+          lnum = symbol.range.start.line + 1,
+          col = symbol.range.start.character + 1,
+          kind = kind,
+          text = text,
+        })
+        if symbol.children then
+          for _, v in ipairs(_symbols_to_items(symbol.children, _items, _bufnr, level + 1)) do
+            vim.list_extend(_items, v)
+          end
+        end
+      end
+    end
+    return _items
+  end
+  return _symbols_to_items(symbols, {}, bufnr, 0)
+end
+
+function M._get_prefix(level, kind)
+  local kind_abbr = config.get_kind_abbr(kind)
+  local spacing = string.rep('  ', level)
+  if kind_abbr == '' then
+    return spacing
+  else
+    return spacing .. '[' .. kind .. '] '
+  end
+end
+
+function M._get_symbol_kind_name(symbol_kind)
+  return protocol.SymbolKind[symbol_kind] or "Unknown"
+end
 
 return M
