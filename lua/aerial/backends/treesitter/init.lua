a9a7247e2ead77538f066f2182c1b7e36f1c9436
Steven Arcangeli <stevearc@stevearc.com>
2021-12-17 13:05:01 -0800

    refactor: simplify treesitter parser logic

--- lua/aerial/backends/treesitter/init.lua
+++ lua/aerial/backends/treesitter/init.lua
@@ -32,74 +32,67 @@ M.fetch_symbols_sync = function(timeout)
     backends.set_symbols(bufnr, items)
     return
   end
+  local lang = parser:lang()
+  local syntax_tree = parser:parse()[1]
+  if not query.has_query_files(lang, "aerial") or not syntax_tree then
+    backends.set_symbols(bufnr, items)
+    return
+  end
   -- This will track a loose hierarchy of recent node+items.
   -- It is used to determine node parents for the tree structure.
   local stack = {}
-  local top_lang = parser:lang()
-  parser:for_each_tree(function(tree, lang_tree)
-    local lang = lang_tree:lang()
-    -- TODO: we don't support parsing symbols from injected languages yet.
-    -- It's hard to put them in the correct position in the symbol tree.
-    if top_lang ~= lang then
-      return
+  local ext = extensions[lang]
+  local kind_map = language_kind_map[lang]
+  for match in query.iter_group_results(bufnr, "aerial", syntax_tree:root(), lang) do
+    local name_node = (utils.get_at_path(match, "name") or {}).node
+    local type_node = (utils.get_at_path(match, "type") or {}).node
+    -- The location capture name is optional. We default to the
+    -- location of the @type capture
+    local loc_node = (utils.get_at_path(match, "location") or {}).node or type_node
+    local parent_item, parent_node, level = ext.get_parent(stack, match, type_node)
+    -- Sometimes our queries will match the same node twice.
+    -- Detect that (type_node == parent_node), and skip dupes.
+    if not type_node or type_node == parent_node then
+      goto continue
     end
-    if not query.has_query_files(lang, "aerial") then
-      return
+    local kind = kind_map[type_node:type()]
+    if not kind then
+      vim.api.nvim_err_writeln(
+        string.format("Missing entry in aerial treesitter kind_map: %s", type_node:type())
+      )
+      break
     end
-    local ext = extensions[lang]
-    local kind_map = language_kind_map[lang]
-    for match in query.iter_group_results(bufnr, "aerial", tree:root(), lang) do
-      local name_node = (utils.get_at_path(match, "name") or {}).node
-      local type_node = (utils.get_at_path(match, "type") or {}).node
-      local loc_node = (utils.get_at_path(match, "location") or {}).node
-      local parent_item, parent_node, level = ext.get_parent(stack, match, type_node)
-      -- Sometimes our queries will match the same node twice.
-      -- If we do (type_node == parent_node), skip all after first.
-      if type_node and type_node ~= parent_node then
-        local kind = kind_map[type_node:type()]
-        if not kind then
-          vim.api.nvim_err_writeln(
-            string.format("Missing entry in aerial treesitter kind_map: %s", type_node:type())
-          )
-          break
-        end
-        if include_kind[kind] then
-          local row, col
-          -- The location capture name is optional. We default to the
-          -- location of the @type capture
-          if loc_node then
-            row, col = loc_node:start()
-          else
-            row, col = type_node:start()
-          end
-          local name
-          if name_node then
-            name = ts_utils.get_node_text(name_node, bufnr)[1] or "<parse error>"
-          else
-            name = "<Anonymous>"
-          end
-          local item = {
-            kind = kind,
-            name = name,
-            level = level,
-            parent = parent_item,
-            lnum = row + 1,
-            col = col,
-          }
-          ext.postprocess(item, match)
-          if item.parent then
-            if not item.parent.children then
-              item.parent.children = {}
-            end
-            table.insert(item.parent.children, item)
-          else
-            table.insert(items, item)
-          end
-          table.insert(stack, { node = type_node, item = item })
-        end
+    if not include_kind[kind] then
+      goto continue
+    end
+    local row, col = loc_node:start()
+    local name
+    if name_node then
+      name = ts_utils.get_node_text(name_node, bufnr)[1] or "<parse error>"
+    else
+      name = "<Anonymous>"
+    end
+    local item = {
+      kind = kind,
+      name = name,
+      level = level,
+      parent = parent_item,
+      lnum = row + 1,
+      col = col,
+    }
+    ext.postprocess(item, match)
+    if item.parent then
+      if not item.parent.children then
+        item.parent.children = {}
       end
+      table.insert(item.parent.children, item)
+    else
+      table.insert(items, item)
     end
-  end)
+    table.insert(stack, { node = type_node, item = item })
+
+    ::continue::
+  end
   backends.set_symbols(bufnr, items)
 end
 
