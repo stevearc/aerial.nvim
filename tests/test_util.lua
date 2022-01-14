e27beaebf802f24fca41e40d0ecde1e2a4b2807c
Steven Arcangeli <stevearc@stevearc.com>
2022-01-14 13:07:14 -0800

    test: improve test failure formatting

--- tests/test_util.lua
+++ tests/test_util.lua
@@ -2,6 +2,30 @@ local data = require("aerial.data")
 local backends = require("aerial.backends")
 local M = {}
 
+local function summarize(received, expected)
+  local lines = { "RECEIVED" }
+  local max_len = 8
+  local function summary(symbol)
+    return string.format("%s %s", symbol.kind, symbol.name)
+  end
+  for _, symbol in ipairs(received) do
+    local s = summary(symbol)
+    max_len = math.max(max_len, string.len(s))
+    table.insert(lines, s)
+  end
+  lines[1] = lines[1] .. string.rep(" ", max_len - string.len(lines[1]) + 4) .. "EXPECTED"
+  for i, symbol in ipairs(expected) do
+    local j = i + 1
+    if lines[j] then
+      local padding = string.rep(" ", max_len - string.len(lines[j]))
+      lines[j] = lines[j] .. padding .. " <> " .. summary(symbol)
+    else
+      lines[j] = string.rep(" ", max_len) .. " <> " .. summary(symbol)
+    end
+  end
+  return table.concat(lines, "\n")
+end
+
 M.test_file_symbols = function(backend_name, filename, expected)
   vim.g.aerial_backends = { backend_name }
   vim.cmd(string.format("edit %s", filename))
@@ -31,33 +55,26 @@ M.assert_tree_equals = function(received, expected, path)
     #expected,
     #received,
     string.format(
-      "Number of symbols at '/%s' do not match %d ~= %d",
+      "Number of symbols at '/%s' do not match %d ~= %d\n%s",
       table.concat(path, "/"),
       #received,
-      #expected
+      #expected,
+      summarize(received, expected)
     )
   )
   for i, child in ipairs(received) do
     local exp_child = expected[i]
-    local err_msg = string.format(
-      [[Symbol mismatch: {
-  kind  = %s, (%s)
-  name  = %s, (%s)
-  level = %s, (%s)
-  lnum  = %s, (%s)
-  col   = %s, (%s)
-}]],
-      child.kind,
-      exp_child.kind,
-      child.name,
-      exp_child.name,
-      child.level,
-      exp_child.level,
-      child.lnum,
-      exp_child.lnum,
-      child.col,
-      exp_child.col
-    )
+    local lines = { "Symbol mismatch: {" }
+    for _, field in ipairs({ "kind", "name", "level", "lnum", "col" }) do
+      local s_field = string.rep(" ", 5 - string.len(field)) .. field
+      local line = string.format("%s = %s", s_field, exp_child[field])
+      if child[field] ~= exp_child[field] then
+        line = line .. string.format("  [%s]", child[field])
+      end
+      table.insert(lines, line)
+    end
+    table.insert(lines, "}")
+    local err_msg = table.concat(lines, "\n")
     local fields = { "kind", "name", "level", "lnum", "col" }
     for _, field in ipairs(fields) do
       assert.equals(exp_child[field], child[field], err_msg)
