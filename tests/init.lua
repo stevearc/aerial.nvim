845a1052f7d8c084eab15d190b62d87350f6fa37
Steven Arcangeli <stevearc@stevearc.com>
2022-01-19 13:04:27 -0800

    test: fix tests for neovim 0.5

--- tests/init.lua
+++ tests/init.lua
@@ -4,6 +4,8 @@ vim.cmd([[runtime! plugin/nvim-treesitter.vim]])
 
 vim.o.swapfile = false
 vim.bo.swapfile = false
+-- Neovim 0.5 doesn't have julia filetype detection
+vim.cmd([[autocmd BufRead,BufNewFile *.jl setfiletype julia]])
 
 require("nvim-treesitter.configs").setup({
   ensure_installed = "maintained",
