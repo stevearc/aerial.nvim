1ce109f0a7054ce54653811da70d79c82841e359
Steven Arcangeli <stevearc@stevearc.com>
2022-04-18 09:29:22 -0700

    ci: fix the build by pinning nvim-treesitter
    It's dropped support for older neovim versions

--- .github/workflows/install_nvim.sh
+++ .github/workflows/install_nvim.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+set -e
 PLUGINS="$HOME/.local/share/nvim/site/pack/plugins/start"
 mkdir -p "$PLUGINS"
 
-wget https://github.com/neovim/neovim/releases/download/${NVIM_TAG}/nvim.appimage
+wget "https://github.com/neovim/neovim/releases/download/${NVIM_TAG}/nvim.appimage"
 chmod +x nvim.appimage
 sudo mv ./nvim.appimage /usr/bin/nvim
 git clone --depth=1 https://github.com/nvim-lua/plenary.nvim.git "$PLUGINS/plenary.nvim"
-git clone --depth=1 https://github.com/nvim-treesitter/nvim-treesitter.git "$PLUGINS/nvim-treesitter"
+git clone https://github.com/nvim-treesitter/nvim-treesitter.git "$PLUGINS/nvim-treesitter"
+pushd "$PLUGINS/nvim-treesitter"
+git reset --hard "$TS_COMMIT"
