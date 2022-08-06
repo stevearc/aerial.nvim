986c580b8c6d5c4f52bcb7f2683ed10049059265
Steven Arcangeli <stevearc@stevearc.com>
2022-08-05 21:58:08 -0700

    cleanup: remove debug prints

--- lua/aerial/backends/lsp/callbacks.lua
+++ lua/aerial/backends/lsp/callbacks.lua
@@ -138,10 +138,6 @@ M.symbol_callback = function(_err, result, context, _config)
   then
     return
   end
-  if bufnr == 1 then
-    print(string.format("symbol callback %s", bufnr))
-    print(string.format("%s", vim.inspect(process_symbols(result, bufnr))))
-  end
 
   -- Debounce this callback to avoid unnecessary re-rendering
   if results[bufnr] == nil then
@@ -167,7 +163,6 @@ M.on_publish_diagnostics = function(_err, result, ctx, _config)
   then
     return
   end
-  -- print(string.format("Set symbols %s: %s", result.uri, bufnr))
 
   -- Don't update if there are diagnostics errors, unless config option is set
   -- or we have no symbols for this buffer
