e4ed6d95f9fe9e45bbc94a773c4354d01654b32d
Steven Arcangeli <stevearc@stevearc.com>
2022-11-27 14:21:57 -0800

    cleanup: remove test logic for old nvim versions

--- tests/minimal_init.lua
+++ tests/minimal_init.lua
@@ -4,12 +4,11 @@ require("nvim-treesitter.query_predicates")
 
 vim.o.swapfile = false
 vim.bo.swapfile = false
--- Neovim 0.5 doesn't have julia filetype detection
-vim.cmd([[autocmd BufRead,BufNewFile *.jl setfiletype julia]])
--- Neovim below 0.7 doesn't have org filetype detection
-vim.cmd([[autocmd BufRead,BufNewFile *.org setfiletype org]])
--- Neovim doesn't have built-in norg filetype detection
-vim.cmd([[autocmd BufRead,BufNewFile *.norg setfiletype norg]])
+vim.filetype.add({
+  extension = {
+    norg = "norg", -- Neovim doesn't have built-in norg filetype detection
+  },
+})
 
 local langs = {}
 for lang, _ in vim.fs.dir("queries") do
