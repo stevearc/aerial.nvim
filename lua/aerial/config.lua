366d0d6f064fae6b83365707b6d92c644ed1e87e
Steven Arcangeli <stevearc@stevearc.com>
2023-01-21 17:44:50 -0800

    doc: fix documentation of keymaps

--- lua/aerial/config.lua
+++ lua/aerial/config.lua
@@ -43,8 +43,8 @@ local default_options = {
 
   -- Keymaps in aerial window. Can be any value that `vim.keymap.set` accepts OR a table of keymap
   -- options with a `callback` (e.g. { callback = function() ... end, desc = "", nowait = true })
-  -- Additionally, if it is a string that matches "aerial.<name>",
-  -- it will use the mapping at require("aerial.action").<name>
+  -- Additionally, if it is a string that matches "actions.<name>",
+  -- it will use the mapping at require("aerial.actions").<name>
   -- Set to `false` to remove a keymap
   keymaps = {
     ["?"] = "actions.show_help",
