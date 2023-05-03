249e78fe4cfbef530a724ad76051c64a07144363
Colin Kennedy <colinvfx@gmail.com>
2023-05-02 22:10:30 -0700

    Simplified existing code

--- queries/usd/aerial.scm
+++ queries/usd/aerial.scm
@@ -1,15 +1,5 @@
-; (prim_definition
-;   (string) @name
-;   (#match? @name "\"([a-zA-Z0-9]+)\"")
-;   (#set! "kind" "Class")
-; ) @type
 (prim_definition
   (string) @name
   (#offset! @name 0 1 0 -1)
   (#set! "kind" "Class")
 ) @type
-; ((identifier) @constant (#offset! @constant 0 1 0 -1))
-; (
-;   (identifier) @constant
-;   (#match? @constant "^[A-Z][A-Z_]+")
-; )
