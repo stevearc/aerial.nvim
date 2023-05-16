f9247b41aab0946657f050f4570d524c26c7c23d
Colin Kennedy <colinvfx@gmail.com>
2023-05-15 21:28:06 -0700

    Added USD-related properties (attributes, relationships, variantSets)

--- queries/usd/aerial.scm
+++ queries/usd/aerial.scm
@@ -3,3 +3,35 @@
   (#offset! @name 0 1 0 -1)
   (#set! "kind" "Class")
 ) @type
+
+(prim_definition
+  (string) @name
+  (#offset! @name 0 1 0 -1)
+  (#set! "kind" "Class")
+) @type
+
+(attribute_assignment
+  (identifier) @name
+  (#set! "kind" "Property")
+) @type
+
+(attribute_declaration
+  (identifier) @name
+  (#set! "kind" "Property")
+) @type
+
+(relationship_assignment
+  (identifier) @name
+  (#set! "kind" "Property")
+) @type
+
+(relationship_declaration
+  (identifier) @name
+  (#set! "kind" "Property")
+) @type
+
+(variant_set_definition
+  (string) @name
+  (#offset! @name 0 1 0 -1)
+  (#set! "kind" "Property")
+) @type
