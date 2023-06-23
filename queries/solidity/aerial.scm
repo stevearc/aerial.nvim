3d4f1e6601a18982b9020900da76d9c3cbe29ba1
Yifan Song <Eric_YF_Song@outlook.com>
2023-06-23 20:48:47 +0200

    Add query for solidity

---
+++ queries/solidity/aerial.scm
@@ -0,0 +1,43 @@
+;; Method and Function declarations
+(contract_declaration (_
+  (function_definition
+    name: (identifier) @name
+    (#set! "kind" "Method")
+    ) @type))
+
+(source_file
+  (function_definition
+    name: (identifier) @name
+    (#set! "kind" "Function")
+    ) @type)
+
+(contract_declaration
+  name: (identifier) @name
+  (#set! "kind" "Class")
+  ) @type
+
+(interface_declaration
+  name: (identifier) @name
+  (#set! "kind" "Interface")
+  ) @type
+
+
+(library_declaration
+  name: (identifier) @name
+  (#set! "kind" "Module")
+  ) @type
+
+(struct_declaration 
+  name: (identifier) @name
+  (#set! "kind" "Struct")
+  ) @type
+
+(enum_declaration 
+  name: (identifier) @name  
+  (#set! "kind" "Enum")
+  ) @type
+
+(event_definition 
+  name: (identifier) @name
+  (#set! "kind" "Class")
+  ) @type
