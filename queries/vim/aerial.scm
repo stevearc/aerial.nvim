ef08437108247d8805ae388f2699537eac2fd810
emmanueltouzery <etouzery@gmail.com>
2023-12-08 05:53:35 +0100

    fix(vim treesitter): support functions with field expression names (#332)

--- queries/vim/aerial.scm
+++ queries/vim/aerial.scm
@@ -3,3 +3,9 @@
     name: [(identifier) (scoped_identifier)] @name)
   (#set! "kind" "Function")
   ) @symbol
+
+(function_definition
+  (function_declaration
+    name: (field_expression) @name)
+  (#set! "kind" "Function")
+  ) @symbol
--- tests/treesitter/vim_test.vim
+++ tests/treesitter/vim_test.vim
@@ -3,3 +3,6 @@ endfunction
 
 function! s:fn_2() abort
 endfunction
+
+function! s:drawer.delete_line() abort
+endfunction
