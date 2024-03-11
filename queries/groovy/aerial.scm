b8935aed00a30fa1e708ac09cd6035c87c09e015
Emmanuel Touzery <etouzery@gmail.com>
2024-03-11 20:20:31 +0100

    groovy support

---
+++ queries/groovy/aerial.scm
@@ -0,0 +1,22 @@
+(juxt_function_call
+  function: (identifier) @name (#not-any-of? @name "else" "try" "exec")
+  (argument_list (closure))
+  (#set! "kind" "Module")
+) @symbol
+
+(declaration
+  name: (identifier) @name
+  (closure)
+  (#set! "kind" "Function")
+) @symbol
+
+(juxt_function_call
+  function: (identifier) @keyword (#eq? @keyword "task")
+  (argument_list ((function_call function: (identifier) @name)))
+  (#set! "kind" "Function")
+) @symbol
+
+(declaration 
+  name: (identifier) @name
+  (#set! "kind" "Constant")
+  ) @symbol
