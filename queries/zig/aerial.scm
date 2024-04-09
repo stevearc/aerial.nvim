6ae7ddcd8d8e9d74714a3f133015227551a0f0d1
Gordon Cassie <gordoncassie@gmail.com>
2024-04-09 13:29:51 -0700

    Add test and tagged unions.

--- queries/zig/aerial.scm
+++ queries/zig/aerial.scm
@@ -9,8 +9,24 @@
             ))))
     )
 
+(Decl
+    (VarDecl
+      variable_type_function: (IDENTIFIER) @name
+      (#set! "kind" "Struct")
+      (_
+        (_
+          (ContainerDecl
+            (ContainerDeclType "union") @symbol
+            ))))
+    )
+
 (FnProto
   function: (IDENTIFIER) @name
   (#set! "kind" "Function")
   ) @symbol
 
+
+(TestDecl
+  (STRINGLITERALSINGLE) @name
+  (#set! "kind" "Function")
+  ) @symbol
