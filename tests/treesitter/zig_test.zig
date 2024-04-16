35df13163db68b213d8b0640d6fa912e31faba9b
Gordon Cassie <gordoncassie@gmail.com>
2024-04-16 08:48:58 -0700

    Add test.

---
+++ tests/treesitter/zig_test.zig
@@ -0,0 +1,13 @@
+fn myFunc() bool {
+    return true;
+}
+
+const MyStruct = struct {
+    is_true: bool,
+};
+
+const MyTaggedUnion = union(enum) { choice1, choice2 };
+
+test "my test" {
+    myFunc();
+}
