5961a1afc0384845934073d3c7d46ea328d98d89
Gordon Cassie <gordoncassie@gmail.com>
2024-04-16 13:27:37 -0700

    feat(zig): basic treesitter support for zig. (#359)
    * Add basic support for navigating zig files.
    
    * Fix struct start position.
    
    * Add test and tagged unions.
    
    * Add test.

---
+++ queries/zig/aerial.scm
@@ -0,0 +1,32 @@
+(Decl
+    (VarDecl
+      variable_type_function: (IDENTIFIER) @name
+      (#set! "kind" "Struct")
+      (_
+        (_
+          (ContainerDecl
+            (ContainerDeclType "struct") @symbol
+            ))))
+    )
+
+(Decl
+    (VarDecl
+      variable_type_function: (IDENTIFIER) @name
+      (#set! "kind" "Struct")
+      (_
+        (_
+          (ContainerDecl
+            (ContainerDeclType "union") @symbol
+            ))))
+    )
+
+(FnProto
+  function: (IDENTIFIER) @name
+  (#set! "kind" "Function")
+  ) @symbol
+
+
+(TestDecl
+  (STRINGLITERALSINGLE) @name
+  (#set! "kind" "Function")
+  ) @symbol
