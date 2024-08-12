263beeb92961c15882b7853805f0ae2024f2e903
WieeRd <verywieerd@gmail.com>
2024-08-13 01:21:46 +0900

    feat(toml): treesitter support for TOML tables (#396)
    * feat(toml): treesitter support for TOML tables
    
    * test(toml): add test data for TOML documents

---
+++ queries/toml/aerial.scm
@@ -0,0 +1,17 @@
+(table
+  [
+    (bare_key)
+    (dotted_key)
+    (quoted_key)
+  ] @name
+  (#set! "kind" "Class")
+) @symbol
+
+(table_array_element
+  [
+    (bare_key)
+    (dotted_key)
+    (quoted_key)
+  ] @name
+  (#set! "kind" "Enum")
+) @symbol
