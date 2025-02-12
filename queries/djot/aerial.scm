fc1e7a7a31ac835c499d3eec300632ee9f7d2c42
Steven Arcangeli <stevearc@stevearc.com>
2025-02-11 17:50:15 -0800

    fix: update djot query for upstream changes

--- queries/djot/aerial.scm
+++ queries/djot/aerial.scm
@@ -1,16 +1,4 @@
 (section
-  [
-    (heading1
-      (content) @name)
-    (heading2
-      (content) @name)
-    (heading3
-      (content) @name)
-    (heading4
-      (content) @name)
-    (heading5
-      (content) @name)
-    (heading6
-      (content) @name)
-  ]
+  (heading
+    content: (content) @name)
   (#set! "kind" "Interface")) @symbol
