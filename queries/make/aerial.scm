003c3e110be4bbf8969c843a53719d247aae2185
Robert Muir <rmuir@apache.org>
2025-08-22 04:55:09 -0400

    feat(make): refine query for make symbols
    Previously the entire list of targets was captured as a single symbol: instead capture each target.
    vim's Makefile is an example that makes use of this feature.
    
    Exclude targets that aren't targets from the "special target list": these are not symbols. This is a
    minor improvement for Makefiles such as the aerial.nvim Makefile, which only declares `.PHONY` a
    single time, but it is a large noise-reducer for cases such as nvim-treesitter Makefile, which lists
    `.PHONY` before each phony target.
    
    Signed-off-by: Robert Muir <rmuir@apache.org>

--- queries/make/aerial.scm
+++ queries/make/aerial.scm
@@ -1,3 +1,11 @@
+; capture each target name from the list
 (rule
-  (targets) @name
-  (#set! "kind" "Interface")) @symbol
+  (targets
+    (_) @name @symbol)
+  ; exclude special names that aren't actually targets
+  ; https://www.gnu.org/software/make/manual/html_node/Special-Targets.html
+  (#not-any-of? @name
+    ".PHONY" ".SUFFIXES" ".DEFAULT" ".PRECIOUS" ".INTERMEDIATE" ".NOTINTERMEDIATE" ".SECONDARY"
+    ".SECONDEXPANSION" ".DELETE_ON_ERROR" ".IGNORE" ".LOW_RESOLUTION_TIME" ".SILENT"
+    ".EXPORT_ALL_VARIABLES" ".NOTPARALLEL" ".ONESHELL" ".POSIX")
+  (#set! "kind" "Interface")) @start
