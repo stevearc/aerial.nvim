62a5a3149b66444a245c052e431c30568ae173e3
Robert Muir <rmuir@apache.org>
2025-08-23 23:49:47 -0400

    fix(c): remove buggy query pattern (#481)
    Reported bug of missing functions (#413) is caused by this query pattern:
    
    ```
    ((declaration) @root @start
      .
      (function_definition) @symbol @end
      (#set! "kind" "Function"))
    ```
    
    Removing the pattern fixes the bug and causes no regressions in tests. The pattern was introduced in
    https://github.com/stevearc/aerial.nvim/commit/91350456c176fe5ef72e342dd3a75f726805454d but it is
    not clear what its purpose is: all functions should be matched by the simple `(function_definition)`
    query immediately after it.
    
    The example from #413 and a few additional tests from tree-sitter-c's test corpus were added.
    
    Closes #413
    
    Signed-off-by: Robert Muir <rmuir@apache.org>

--- tests/treesitter/c_test.c
+++ tests/treesitter/c_test.c
@@ -17,3 +17,33 @@ typedef enum {
 typedef struct {
   int field;
 } St_1;
+
+int var = 5;
+
+int foo() {
+  return 0;
+}
+
+int bar() {
+  return 0;
+}
+
+// K&R style
+int fn_kr(bar, baz, qux)
+int bar, baz;
+char *qux;
+{
+}
+
+// specifiers after types
+int static inline fn_specifiers(int arg1) {
+  return 5;
+}
+
+// fn definition with macro attributes
+void * fn_macro_attributes(int arg1)
+  SOME_ATTR
+  SOME_ATTR(1)
+{
+  return 5;
+}
