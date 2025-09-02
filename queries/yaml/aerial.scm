77cc0b8e980a5eb74e88eedbe64863121d887096
Robert Muir <rmuir@apache.org>
2025-09-02 02:10:45 -0400

    feat(yaml): treesitter name array elements symbol based upon "name" key
    Previously, these elements were not listed as symbols. redhat's yaml-language-server lists these as
    numbered elements such as 0, 1, 2, but treesitter strategy didn't match them at all.
    
    There's a de-facto standard for many yaml formats, to use the "name" key to solve this problem. It
    is a best practice for commonly used yaml files such as github workflows and ansible.
    
    Signed-off-by: Robert Muir <rmuir@apache.org>

--- queries/yaml/aerial.scm
+++ queries/yaml/aerial.scm
@@ -9,3 +9,19 @@
   value: (block_node
     (block_sequence) @symbol)
   (#set! "kind" "Enum")) @start
+
+; "array element" with a "name:" key
+; ansible play/task names, etc
+; github workflow/actions jobs/steps, etc
+(block_sequence_item
+  (block_node
+    (block_mapping
+      (block_mapping_pair
+        key: (flow_node
+          (plain_scalar
+            (string_scalar) @_key))
+        value: (flow_node
+          (plain_scalar
+            (string_scalar) @name)))))
+  (#eq? @_key "name")
+  (#set! "kind" "Module")) @symbol
