2da89aefc515314c710194974c40e91289b9b987
Ruslan Kovalenko <rkovalenko.m5@gmail.com>
2026-04-16 22:25:59 +0200

    fix: use node:range() instead of deprecated node:start()

--- lua/aerial/backends/treesitter/helpers.lua
+++ lua/aerial/backends/treesitter/helpers.lua
@@ -10,12 +10,11 @@ end
 ---@param end_node TSNode
 ---@return aerial.Range
 M.range_from_nodes = function(start_node, end_node)
-  local row, col = start_node:start()
-  local end_row, end_col = end_node:end_()
+  local start_row, start_col, end_row, end_col = start_node:range()
   return {
-    lnum = row + 1,
+    lnum = start_row + 1,
     end_lnum = end_row + 1,
-    col = col,
+    col = start_col,
     end_col = end_col,
   }
 end
