ae1342017c2e1232c6bc0a2449f048e46ad1beb5
Kacper Zielinski <KacperZ155@gmail.com>
2026-06-02 05:08:23 +0200

    feat(cpp): add support for namespace symbols
    Add support for `namespace` declarations in C++.
    https://en.cppreference.com/cpp/language/namespace
    
    Note: `Namespace` symbols are hidden by default.

--- queries/cpp/aerial.scm
+++ queries/cpp/aerial.scm
@@ -1,3 +1,24 @@
+(namespace_definition
+  name: (namespace_identifier) @name
+  body: (declaration_list) @symbol
+  (#set! "kind" "Namespace"))
+
+; Nested namespace definition (since C++17)
+(namespace_definition
+  name: (nested_namespace_specifier) @name
+  body: (declaration_list) @symbol
+  (#set! "kind" "Namespace"))
+
+; Namespace alias
+(namespace_alias_definition
+  name: (namespace_identifier) @name
+  (#set! "kind" "Namespace")) @symbol
+
+; Anonymous namespace
+(namespace_definition
+  body: (declaration_list) @symbol
+  (#set! "kind" "Namespace"))
+
 (struct_specifier
   name: (type_identifier) @name
   body: (field_declaration_list)
--- tests/treesitter/cpp_test.cpp
+++ tests/treesitter/cpp_test.cpp
@@ -5,6 +5,28 @@ struct st_1 {};
 struct {
 } st_2;
 
+namespace n_1 {
+  struct st_n {};
+}
+using namespace n_1;  // This should not show up as a symbol
+
+namespace n_2a {
+  namespace n_2b {
+    struct st_nn {};
+  }
+}
+using n_2a::n_2b::st_nn;  // This should not show up as a symbol
+
+namespace n_a::n_b::n_c {
+  struct st_n {};
+}
+
+namespace n_abc = n_cba;
+
+namespace {
+  struct st_n {};
+}
+
 enum en_1 {};
 
 class cl_1 {
